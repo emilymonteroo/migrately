7992c69411002711fbc1f823c581321a1c59cd42
Emily Montero <114720218+emilymonteroo@users.noreply.github.com>
2023-02-08 16:01:56 -0800

    first commit

---
+++ react/routes/DashboardRoutes.jsx
@@ -0,0 +1,21 @@
+import { v4 as uuid } from "uuid";
+
+export const DashboardMenu = [
+  
+  {
+    id: uuid(),
+    title: "Attorney Licenses",
+    roles: ["Admin", "User", "Attorney"],
+    icon: "book",
+    link: "/licenses",
+  },
+  {
+    id: uuid(),
+    title: "Add New Attorney License",
+    roles: ["Admin", "Attorney"],
+    icon: "book",
+    link: "/licenses/new",
+  },
+  
+];
+export default DashboardMenu;
